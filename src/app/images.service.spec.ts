587d3dbc06511cd8b0070bf5f1698fcc74fff10b
yingrui <yingrui.f@gmail.com>
2016-10-17 23:34:34 +0800

    @yingrui: add http service test

--- src/app/images.service.spec.ts
+++ src/app/images.service.spec.ts
@@ -2,17 +2,35 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ImagesService } from './images.service';
-import {HttpModule} from "@angular/http";
+
+import {
+  MockBackend,
+  MockConnection
+} from '@angular/http/testing';
+
+import {
+  HttpModule, XHRBackend, Response, ResponseOptions
+} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 describe('Service: Images', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ImagesService],
+      providers: [
+        ImagesService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ],
       imports: [HttpModule],
     });
   });
 
-  it('should ...', inject([ImagesService], (service: ImagesService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should ...', async(inject([ImagesService, XHRBackend], (service: ImagesService, backend: MockBackend) => {
+    let options = new ResponseOptions({status: 200, body: {repositories: ['ubuntu']}});
+    let response = new Response(options);
+    backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+    service.getImages().then(images => {
+      expect(images).toEqual(['ubuntu']);
+    });
+  })));
 });
