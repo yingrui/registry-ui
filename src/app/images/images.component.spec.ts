19e92028d70b88a4ece290b8e9b3ae684e0277fa
yingrui <yingrui.f@gmail.com>
2016-10-18 12:23:14 +0800

    @yingrui: add test for displaying all images on page

--- src/app/images/images.component.spec.ts
+++ src/app/images/images.component.spec.ts
@@ -1,14 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
 import {TestBed, async, ComponentFixture} from '@angular/core/testing';
-import { ImagesComponent } from './images.component';
-import { ImagesService } from "../images.service";
+import {ImagesComponent} from './images.component';
+import {ImagesService} from '../images.service';
+import {By} from '@angular/platform-browser';
 import 'rxjs/add/operator/toPromise';
+import * as _ from 'lodash';
 
 describe('Component: Containers', () => {
 
   let fixture: ComponentFixture<ImagesComponent>;
-  let component:    ImagesComponent;
+  let component: ImagesComponent;
 
   beforeEach(() => {
     let imagesServiceStub = {
@@ -27,7 +29,13 @@ describe('Component: Containers', () => {
   it('should return a list of images in docker registry', async(() => {
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      expect(component.images).toEqual(['ubuntu']);
+      let expectedImages = ['ubuntu'];
+      expect(component.images).toEqual(expectedImages);
+      fixture.detectChanges();
+      let imageElements = fixture.debugElement.queryAll(By.css('li')).map(de => de.nativeElement);
+      expect(imageElements.length).toBe(expectedImages.length);
+      _.zip(imageElements, expectedImages)
+        .forEach(imageAndName => expect(imageAndName[0].textContent).toEqual(imageAndName[1]));
     });
   }));
 });
